bd1d713004036f0cef7233d71adcdff3516cc54a
Pumlani Kewana <pumlanibasil15@gmail.com>
2024-06-21 13:47:18 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './Components/HomePage';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Latest Movies</h1>
       <HomePage />
+      <Footer />
     </div>
   );
 }
