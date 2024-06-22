813204d767a76ec93a96da025385b9bdd141c856
Pumlani Kewana <pumlanibasil15@gmail.com>
2024-06-22 16:34:30 +0200

    added a slider to home page for nicer design

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,41 +1,64 @@
+// src/components/LatestMovies.js
 import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
 import { fetchLatestMovies } from '../utils/apiConfig';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import '../Components/HomePage.css';
-import Footer from '../Components/Footer.css';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const getMovies = async () => {
-      try {
-        const movies = await fetchLatestMovies();
-        console.log(movies)
-        setMovies(movies);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
+    const getLatestMovies = async () => {
+      const latestMovies = await fetchLatestMovies();
+      setMovies(latestMovies);
     };
 
-    getMovies();
+    getLatestMovies();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error fetching movies.</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
-    <div className="movies-container">
-      {movies.map(movie => (
-        <div key={movie.id} className="movie-card">
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-          <h2>{movie.title}</h2>
-          <p>{movie.overview}</p>
-        </div>
-      ))}
+    <div>
+      <h1>Latest Movies</h1>
+      <Slider {...settings}>
+        {movies.map((movie) => (
+          <div key={movie.id} className="movie-card">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
